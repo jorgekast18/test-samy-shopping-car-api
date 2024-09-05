655d12fb7489f6250e5e4fa3555f6e28c148f6e5
jorge <jorge@JORGE.jorgecastano.online>
2024-09-05 16:35:29 -0500

    Add global prefix for v1 api

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,9 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import * as dotenv from 'dotenv';
 
-dotenv.config({ path: '.env' });
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.setGlobalPrefix('api/v1');
   await app.listen(3000);
 }
 bootstrap();
