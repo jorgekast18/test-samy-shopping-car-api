965a63513fccbff54ce375a59ecda4c2d8c36ec3
jorge <jorge@JORGE.jorgecastano.online>
2024-09-05 16:57:16 -0500

    Set config swagger

--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,28 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { HttpExceptionFilter } from './common/filters/http-exception.filter';
 import { ValidationPipe } from './common/pipes/validation.pipe';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.setGlobalPrefix('api/v1');
+
+  // Config swagger
+  const config = new DocumentBuilder()
+    .setTitle('Samy Challenge - Carrito de Compras API')
+    .setDescription('API para gestionar un carrito de compras')
+    .setVersion('1.0')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+  
+  // Filters and pipes
   app.useGlobalFilters(new HttpExceptionFilter());
   app.useGlobalPipes(new ValidationPipe());
+
+  // Set global prefix
+  app.setGlobalPrefix('api/v1');
   await app.listen(3000);
 }
 bootstrap();
