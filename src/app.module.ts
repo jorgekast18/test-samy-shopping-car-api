aebc5c1b9615e6c68792486bf561b507c8642822
jorge <jorge@JORGE.jorgecastano.online>
2024-09-05 14:46:28 -0500

    Modify for add config for database connection

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,15 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { MongooseModule } from '@nestjs/mongoose';
+import { databaseConfig } from './config';
 
 @Module({
-  imports: [],
+  imports: [
+    MongooseModule.forRootAsync({
+      useFactory: async () => (databaseConfig)
+    })
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
